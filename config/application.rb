b7df709b73cae49f4bcf73e818a3e6a66f46d31c
Dennis de Vulder <dennisdevulder@gmail.com>
2014-10-10 21:31:17 +0200

    Print reachable address

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,22 @@ require "torrent_api"
 require "omxplayer"
 require "deluge"
 
+require 'socket'
+
 class Application < Sinatra::Base
   enable :sessions
   Tmdb::Api.key('2433ca02f76e3becd9e1411ca69a028a')
   DAEMON = Deluge.new
   DAEMON.login 'admin', 'admin'
+
+  orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true
+  UDPSocket.open do |s|
+    s.connect '64.233.187.99', 1
+    puts "Connection available at  http://#{s.addr.last}:4567"
+  end
+
+  Socket.do_not_reverse_lookup = orig
+
 
   require './models/movie.rb'
   require './config/routes'
